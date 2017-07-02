46b2458f7affe0100b454a6cf2e885db73475f46
Jackson Achankunju <jachankunju@noon.com>
2017-07-02 15:07:23 +0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,54 @@ var path = require('path');
 var autoprefixer = require('autoprefixer-core');
 
 module.exports = {
-  devtool: '#inline-source-map',
-  entry: {
-    'docs.js': [
-      './docs/index.jsx',
-      // 'webpack/hot/only-dev-server',
-      // 'webpack-dev-server/client?http://localhost:8000'
+    devtool: '#inline-source-map',
+    entry: {
+        'docs.js': [
+            './docs/index.jsx'
+            // 'webpack/hot/only-dev-server',
+            // 'webpack-dev-server/client?http://localhost:8000'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name]'
+    },
+    module: {
+        rules: [
+            { test: /\.jsx$/, loaders: ['babel'] },
+            { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
+            {
+                test: /\.scss$/,
+                use: [
+                    require.resolve('style-loader'),
+                    require.resolve('css-loader'),
+                    {
+                        loader: require.resolve('postcss-loader'),
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [autoprefixer({ browsers: ['last 2 version'] })]
+                        }
+                    },
+                    {
+                        loader: require.resolve('sass-loader'),
+                        options: {
+                            outputStyle: 'expanded&' + 'includePaths[]=' + path.resolve(__dirname, './node_modules')
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.md$/,
+                use: [require.resolve('html-loader'), require.resolve('markdown-loader')]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        // new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.IgnorePlugin(/vertx/)
     ]
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name]'
-  },
-  module: {
-    loaders: [
-      {test: /\.jsx$/, loaders: ['babel']},
-      {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/},
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass?outputStyle=expanded&' + 'includePaths[]=' +
-                  (path.resolve(__dirname, './node_modules'))
-      },
-      { test: /\.md$/, loader: 'html!markdown' }
-    ]
-  },
-  postcss: [ autoprefixer({ browsers: ['last 2 version'] }) ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  plugins: [
-    // new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.IgnorePlugin(/vertx/)
-  ]
 };
