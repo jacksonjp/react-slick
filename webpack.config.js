696db9fa0241c63418a877fb05ec01c6c5058aff
Jackson Achankunju <jachankunju@noon.com>
2017-07-02 15:12:12 +0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     },
     plugins: [
         // new webpack.HotModuleReplacementPlugin(),
